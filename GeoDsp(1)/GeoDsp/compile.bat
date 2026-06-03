afd3f43afcf48f12b4fd3a79e222801130f290d4
kkazusa <2231754412@qq.com>
2026-06-03 17:25:13 +0800

    Fix scripts to use JDK 17 explicitly
    JAVA_HOME defaults to JDK 8, but Spring Boot 3.4.0 requires JDK 17.
    Both scripts now set JAVA_HOME to JDK 17 before running.

--- GeoDsp(1)/GeoDsp/compile.bat
+++ GeoDsp(1)/GeoDsp/compile.bat
@@ -1,4 +1,6 @@
 @echo off
+set "JAVA_HOME=C:\Program Files\Java\jdk-17.0.18"
+set "PATH=%JAVA_HOME%\bin;%PATH%"
 cd /d "%~dp0"
 call mvnw compile
 pause
--- start_backend.bat
+++ start_backend.bat
@@ -1,2 +1,4 @@
 @echo off
+set "JAVA_HOME=C:\Program Files\Java\jdk-17.0.18"
+set "PATH=%JAVA_HOME%\bin;%PATH%"
 start "GeoDspBackend" java -jar "%~dp0GeoDsp(1)\GeoDsp\target\dsp-0.0.1-SNAPSHOT.jar" --server.port=9966
